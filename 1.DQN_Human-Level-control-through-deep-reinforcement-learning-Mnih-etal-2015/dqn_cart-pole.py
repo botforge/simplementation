b4d860b1a86f258615a1f34729c065acab33189f
botforge <botforge@github.com>
2019-06-26 21:45:53 -0400

    Finish Simple_DQN nnModule

--- 1.DQN_Human-Level-control-through-deep-reinforcement-learning-Mnih-etal-2015/dqn_cart-pole.py
+++ 1.DQN_Human-Level-control-through-deep-reinforcement-learning-Mnih-etal-2015/dqn_cart-pole.py
@@ -12,9 +12,9 @@ device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
 class Simple_ReplayMemory(object):
     def __init__(self, size=10000, batch_size=32, obs_space_shape = 4):
         """
-        Simple Replay Memory implementation for low-dimensional data observations (eg. Classic Controle envs)
+        Simple Replay Memory implementation for low-dimensional data observations (eg. Classic Control envs) 
 
-        NOTE: This implementation does not "stack frames" together as described by <agent_history_length> hyperparamter (https://www.nature.com/articles/nature14236.pdf), Mihn et al. 2015
+        NOTE: This implementation does not "stack frames" together as described by <agent_history_length> hyperparamter (https://www.nature.com/articles/nature14236.pdf), Mnih et al. 2015
         Stacking frames is unneccesary for low dimensional observations, like CartPole which is simply of type Box(4,)
 
         Parameters
@@ -26,21 +26,20 @@ class Simple_ReplayMemory(object):
         obs_space_shape: int
             The size of the vector representing the observation space 
         """
-        self.size, self.batch_size, self.obs_space_shape = size, batch_size,obs_space_shape
+        super(Simple_ReplayMemory, self).__init__()
 
-        self.memory = [] #stores the observations
-        self.count = 0 #keep track of how many experiences you have
-        self.idx = 0 #and which experience you're on
+        self.max_size, self.batch_size, self.obs_space_shape = size, batch_size,obs_space_shape
 
-        #Pre-allocate arrays that return samples
-        self.obs_minibatch = np.empty((self.batch_size, self.obs_space_shape))
-        self.actions_minibatch = np.empty((self.batch_size,))
-        self.rewards_minibatch = np.empty((self.batch_size, ))
-        self.next_obs_minibatch = self.obs_minibatch.copy()
-        self.done_minibatch = np.empty((self.batch_size, ))
+        #Pre-allocate arrays that represent the state of the Replay Memory
+        self.observations = np.empty((self.max_size, obs_space_shape), dtype=np.float32)
+        self.actions = np.empty((self.max_size, 1), dtype=np.int32)
+        self.rewards = np.empty((self.max_size, 1), dtype=np.float32)
+        self.done_flags = np.empty((self.max_size, 1), dtype=np.bool)
 
+        self.count = 0 
+        self.idx = 0 
 
-    def add(self, obs, action, reward, next_obs, done):
+    def add(self, obs, action, reward, done):
         """
         Add an experience to replay memory
         Parameters
@@ -51,71 +50,126 @@ class Simple_ReplayMemory(object):
             An action from Gym environment of shape (<env.action_space.n>, )
         reward: float
             Reward for calling env.step(action) on obs
-        next_obs: np.array
-            Next observation from calling env.step(action) on obs
         done: boolean
             Boolean stating whether the episode terminated (also from calling env.step(action) on obs)
         """
-        data = (obs, action, reward, next_obs, done)
 
-        #Checks if current index is initialized in memory (if it has been initialized, overwrite)
-        if self.idx >= len(self.memory):
-            self.memory.append(data)
-        else:
-            self.memory[self.idx] = data
-
+        #Update state of Replay Memory to include new information
+        self.observations[self.count, ...] = obs
+        self.actions[self.count, 0] = action
+        self.rewards[self.count, 0] = reward
+        self.done_flags[self.count, 0] = done
         self.count = max(self.count, self.idx+1) 
-        self.idx = (self.idx+1) % self.size #if self.idx == self.size, resets to zero
-        
+        self.idx = (self.idx+1) % self.max_size #if self.idx == self.max_size, resets to zero
 
     def sample(self):
         """ Sample a random minibatch of states
         (default implementation is batch_size=32 & env=CartPole-v1)
+
         Returns:
         -------
         obs_minibatch: np.array
-            Shape (<self.batch_size>, <agent_history_length>, <self.obs_space_shape>) - default implementation : (32, 4, 1) 
+            Shape (<self.batch_size>, <self.obs_space_shape>) - default implementation : (32, 4)
         actions_minibatch: np.array
             Shape (<self.batch_size>, <env.action_space.n>) - default implementation : (32,1)
         rewards_minibatch: np.array
-            Shape (<self.batch_size>, ) - default implementation: (32,1)
+            Shape (<self.batch_size>, 1) - default implementation: (32,1)
         next_obs_minibatch: np.array
             Same as obs_minibatch
         done_minibatch: np.array
-            Shape (<self.batch_size>, ) - default implementation: (32,1)
+            Shape (<self.batch_size>, 1) - default implementation: (32,1)
         """
 
-        #Sample array of random indicies [agent_history_length-1, agent_history_length, ...., self.count] without repetition
-        sampling_idxs = random.sample(range(self.agent_history_length-1, self.count), self.batch_size) 
+        #Sample array of random indicies [0, 1, 2,...., self.count-1] without repetition
+        sampling_idxs = np.array(random.sample(range(0, self.count), self.batch_size))
+        next_idxs = sampling_idxs + 1
+        invalid_next_idxs = np.nonzero(next_idxs >= self.count)[0]
+        next_idxs[invalid_next_idxs] = -1
 
-        for i, sample_idx in enumerate(sampling_idxs):
+        obs_minibatch = self.observations[sampling_idxs, ...]
 
-            #Stack current sampled observation (sample_idx) + the past <self.agent_history_length> sampled observations into the minibatch
-            start_idx = sample_idx + 1 - self.agent_history_length
-            end_idx = sample_idx + 1
-            history_samples = self.memory[start_idx:end_idx] #returns a list of tuples (obs, action, reward, next_obs, done) of length <self.agent_history_length>
+        #Fix next_obs_minibatch by zeroing out invalid observations
+        next_obs_minibatch = self.observations[next_idxs, ...]
+        next_obs_minibatch[invalid_next_idxs, ...] = np.zeros(self.obs_space_shape)
 
-            #Iterate through list and update minibatch with history samples
-            for j, data_sample in enumerate(history_samples):
-                self.obs_minibatch[i,j,...] = data_sample[0]
-                self.next_obs_minibatch[i,j,...] = data_sample[3]
+        actions_minibatch = self.actions[sampling_idxs, ...]
+        rewards_minibatch = self.rewards[sampling_idxs, ...]
+        done_minibatch = self.done_flags[sampling_idxs, ...]
+        return obs_minibatch, next_obs_minibatch, actions_minibatch, rewards_minibatch, done_minibatch
 
-            #The action, rewards, and done minibatches don't care about stacking history samples. We can directly index into the current sample
-            curr_sample = self.memory[sample_idx]
-            self.actions_minibatch[i] = curr_sample[1]
-            self.rewards_minibatch[i] = curr_sample[2]
-            self.done_minibatch[i] = curr_sample[4]
+def _weights_init(m):
+    if hasattr(m, 'weight'):
+        nn.init.kaiming_uniform_(m.weight, a=0, nonlinearity='relu')
+    if hasattr(m, 'bias'):
+        nn.init.constant(m.bias, 0)
 
-            return self.obs_minibatch, self.next_obs_minibatch, self.actions_minibatch[None], self.rewards_minibatch[None], self.done_minibatch[None]
-            
+class Simple_DQN(nn.Module):
+    def __init__(self, obs_space_shape, action_space_shape):
+        """
+        Neural Network that predicts the Q-value for all actions "a_t" given an input state "s_t" for low dimensional action space
+        The Q-Value is the output function Q(s_t, a_t) that estimates the expected future reward of taking an action "a_t" in state "s_t"
+        For the CartPole problem, this function will predict the expected future reward of "moving left" and "moving right" given 4 joint states.
+        Visit: http://rail.eecs.berkeley.edu/deeprlcourse/static/slides/lec-7.pdf for more info
+
+        Parameters
+        ----------
+        obs_space_shape:int 
+            The size of the vector representing the observation space (for Cartpole = 4)
+        action_space_shape:int
+            The size of the vector representing the action space (for Cartpole = 2)
+        """
+        super(Simple_DQN, self).__init__()
+
+        #Build a simple Linear modell
+        self.model = nn.Sequential(
+            nn.Linear(obs_space_shape, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, action_space_shape)
+        )
+
+        #He Initialization
+        self.apply(_weights_init)
+
+    def forward(self, obs):
+        """
+        Run a forward pass through Neural Network
+        
+        Parameters
+        ----------
+        obs: np.array
+            Observations of shape (batch_size, <obs_space_shape>)
+        
+        Returns
+        -------
+        act: np.array
+            Actions of shape (batch_size, <action_space_shape>)
+        """
+        return self.model(obs)
+    
 def main():
-    #Make OpenAI gym environment
+    #Make OpenAI gym environment (we only consider discrete binary action spaces)
     env = gym.make('CartPole-v1')
     obs_space_shape = env.observation_space.shape[0]
-    #We alter the default hyperparameters presented in Mihn et al. due to simplicity of the Cart-Pole environment
-    replay_memory = ReplayMemory(size=10000, agent_history_length=1, batch_size=32, obs_space_shape=obs_space_shape)
-    
+    action_space_shape = env.action_space.n
 
+    #Make Q-network and Target Q-network
+    q_net = 
+
+    #Set random seeds
+    seed = random.randint(0, 9999)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available:
+        torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    env.seed(seed)
+
+    #We alter the default hyperparameters presented in Mihn et al. due to low-dimensionality of the Cart-Pole environment
+    replay_memory = Simple_ReplayMemory(size=10000, batch_size=32, obs_space_shape=obs_space_shape)
+    
+    
 if __name__ == "__main__":
     main()
 
